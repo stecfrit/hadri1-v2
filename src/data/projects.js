f8e1d36c94dddb19e128617810fde06018da6458
hadri12velay <hadri1.2velay@gmail.com>
2024-04-18 17:42:59 +0100

    added the orbit calculator project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,23 +1,36 @@
 export const projects = [
     {
+        'name': 'Orbital Calculator',
+        'moniker': 'orbit',
+        'description':
+            'An app to calculate some simple manoeuvres. This is a personal challenge to review some orbital mechanics fundamentals, as well as implement a robust calculating and plotting tool into a React Vite app. For the moment, it only includes a circular Hohmann transfer, but I am working to introduce tools for elliptic and bi-elliptic Hohmann transfers',
+        'tech': ['React', 'Vite'],
+        'link': 'https://orbit.hadri1.com',
+        'preview_image': 'not-available.png',
+        'new': true,
+        'show': true,
+    },
+    {
         'name': 'Chess for iPhone',
         'moniker': 'chess',
-        'description': "Chess for 2 players on one device, this was made for iPhones, and is used as a Progressive Web App (PWA) by clicking 'Add to Homescreen' from Safari. I wanted to use Vite for this project to create my React app, as I had never used it and had too many problems with create-react-app.",
+        'description':
+            "Chess for 2 players on one device, this was made for iPhones, and is used as a Progressive Web App (PWA) by clicking 'Add to Homescreen' from Safari. I wanted to use Vite for this project to create my React app, as I had never used it and had too many problems with create-react-app.",
         'tech': ['React', 'Vite'],
         'link': 'https://chess.hadri1.com',
         'preview_image': 'chess.png',
         'new': true,
-        'show': true
+        'show': true,
     },
     {
         'name': 'Luminance Company Website',
         'moniker': 'luminance',
-        'description': 'As part of my role as a Web Software Engineer at Luminance, one of my projects was to lead the development of the new company website design. ',
+        'description':
+            'As part of my role as a Web Software Engineer at Luminance, one of my projects was to lead the development of the new company website design. ',
         'tech': ['Handlebars'],
         'link': 'https://www.luminance.com',
         'preview_image': 'luminance.png',
         'new': false,
-        'show': true
+        'show': true,
     },
     {
         'name': 'Test',
@@ -27,27 +40,29 @@ export const projects = [
         'link': 'https://zeitgeist.hadri1.com',
         'preview_image': 'zeitgeist.png',
         'new': false,
-        'show': true
+        'show': true,
     },
     {
         'name': 'Photo Gallery',
         'moniker': 'gallery',
-        'description': 'This is an old photo gallery website I made for a friend, using simple HTML and CSS, when I was starting out in front-end web development.',
+        'description':
+            'This is an old photo gallery website I made for a friend, using simple HTML and CSS, when I was starting out in front-end web development.',
         'tech': [],
         'link': 'https://gallery.hadri1.com',
         preview_image: 'gallery.png',
         'new': false,
-        'show': true
+        'show': true,
     },
     {
         'name': 'Big Brain',
         'moniker': 'big-brain',
-        'description': "AI text-completion app, with customizable settings, using OpenAI's API.",
+        'description':
+            "AI text-completion app, with customizable settings, using OpenAI's API.",
         'tech': ['React', 'OpenAI', 'Axios'],
         'link': 'https://brain.hadri1.com',
         'preview_image': 'brain.png',
         'new': false,
-        'show': true
+        'show': true,
     },
     // {
     //     'name': 'Silhouette',
