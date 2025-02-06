a8762e9fa4745e28ec1a95e8d0e6ffe6160efacb
hadri12velay <hadri1.2velay@gmail.com>
2025-02-06 13:44:44 +0100

    added a better theme

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,42 +1,37 @@
-import React, { useContext, useEffect } from 'react';
-import { ThemeContext } from './contexts/ThemeContext';
+import React, { useContext, useEffect } from "react";
+import { ThemeContext } from "./contexts/ThemeContext";
 
 export default function Theme() {
-    const { theme, setTheme } = useContext(ThemeContext);
+  const { theme, setTheme } = useContext(ThemeContext);
 
-    const localStorage = window.localStorage;
+  const localStorage = window.localStorage;
 
-    useEffect(() => {
-        document.documentElement.setAttribute('data-theme', theme);
-        localStorage.setItem('globalTheme', theme);
-    }, [theme]);
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("globalTheme", theme);
+  }, [theme]);
 
-    return (
-        <div id="Theme">
-            <div
-                className="theme-button default"
-                onClick={() => setTheme('default')}
-            ></div>
-            <div
-                className="theme-button red"
-                onClick={() => setTheme('red')}
-            ></div>
-            {/* <div
-                className="theme-button blue"
-                onClick={() => setTheme('blue')}
-            ></div> */}
-            <div
-                className="theme-button blue2"
-                onClick={() => setTheme('blue2')}
-            ></div>
-            <div
-                className="theme-button brown"
-                onClick={() => setTheme('brown')}
-            ></div>
-            <div
-                className="theme-button blue3"
-                onClick={() => setTheme('blue3')}
-            ></div>
-        </div>
-    );
+  return (
+    <div id="Theme">
+      <div className="theme-button gold" onClick={() => setTheme("gold")}></div>
+      <div
+        className="theme-button green"
+        onClick={() => setTheme("green")}
+      ></div>
+      <div className="theme-button red" onClick={() => setTheme("red")}></div>
+      {/* <div className="theme-button blue" onClick={() => setTheme("blue")}></div> */}
+      {/* <div
+        className="theme-button blue2"
+        onClick={() => setTheme("blue2")}
+      ></div> */}
+      <div
+        className="theme-button brown"
+        onClick={() => setTheme("brown")}
+      ></div>
+      {/* <div
+        className="theme-button blue3"
+        onClick={() => setTheme("blue3")}
+      ></div> */}
+    </div>
+  );
 }
--- src/components/contexts/ThemeContext.jsx
+++ src/components/contexts/ThemeContext.jsx
@@ -1,32 +1,32 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from "react";
 
 const localStorage = window.localStorage;
-const savedThemeLocal = localStorage.getItem('globalTheme');
+const savedThemeLocal = localStorage.getItem("globalTheme");
 
-const initial_theme = !!savedThemeLocal ? savedThemeLocal : 'default';
+const initial_theme = !!savedThemeLocal ? savedThemeLocal : "gold";
 
 export const ThemeContext = createContext({
-    theme: initial_theme,
-    setTheme: () => {},
+  theme: initial_theme,
+  setTheme: () => {},
 });
 
 const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState(initial_theme);
+  const [theme, setTheme] = useState(initial_theme);
 
-    const setThemeHandler = (chosen_theme) => {
-        setTheme(chosen_theme);
-    };
+  const setThemeHandler = (chosen_theme) => {
+    setTheme(chosen_theme);
+  };
 
-    return (
-        <ThemeContext.Provider
-            value={{
-                theme: theme,
-                setTheme: setThemeHandler,
-            }}
-        >
-            {children}
-        </ThemeContext.Provider>
-    );
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme: theme,
+        setTheme: setThemeHandler,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
 export default ThemeProvider;
