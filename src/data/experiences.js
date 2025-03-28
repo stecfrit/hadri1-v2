11f4a57c2194bc5ccdd16972f30687deba10dfe2
hadri12velay <hadri1.2velay@gmail.com>
2025-03-28 10:41:40 +0100

    Added some stuff

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,21 +1,31 @@
 export const experiences = [
-    {
-        company: 'Luminance',
-        role: 'Web Softare Engineer',
-        start: '09/22',
-        end: '01/24',
-        description:
-            'Led independent overhaul of company website, tested and deployed while coordinating with Marketing. Developed mostly Front-End components for single-page app, fostering cross-functional collaboration for efficient debugging and testing. Provided mentorship to new team members, sharing deep insights into the codebase and best practices, fostering rapid onboarding and professional development.',
-        skills: ['Typescript', 'Git', 'PostgreSQL', 'Node'],
-        logo: 'luminance-logo.png',
-    },
-    {
-        company: 'UoS Skydive Cub',
-        role: 'Vice-President & PR Officer',
-        start: '09/19',
-        end: '06/22',
-        description:
-            'Leadership experience from being the Vice-President and Public Relations Officer of the University of Southampton Skydive Club. Developed resilience from adapting to challenges introduced by the pandemic.',
-        skills: [],
-    },
+  {
+    company: "DataAnnotation",
+    role: "Software Validation Engineer",
+    start: "07/24",
+    end: "now",
+    description:
+      "Critically analyse and rank AI model responses to coding and STEM related user prompts. Create and test coding responses to train AI models.",
+    skills: [],
+    logo: "luminance-logo.png",
+  },
+  {
+    company: "Luminance",
+    role: "Web Softare Engineer",
+    start: "09/22",
+    end: "01/24",
+    description:
+      "Led independent overhaul of company website, tested and deployed while coordinating with Marketing. Developed mostly Front-End components for single-page app, fostering cross-functional collaboration for efficient debugging and testing. Provided mentorship to new team members, sharing deep insights into the codebase and best practices, fostering rapid onboarding and professional development.",
+    skills: ["Typescript", "Git", "PostgreSQL", "Node"],
+    logo: "luminance-logo.png",
+  },
+  {
+    company: "UoS Skydive Cub",
+    role: "Vice-President & PR Officer",
+    start: "09/19",
+    end: "06/22",
+    description:
+      "Leadership experience from being the Vice-President and Public Relations Officer of the University of Southampton Skydive Club. Developed resilience from adapting to challenges introduced by the pandemic.",
+    skills: [],
+  },
 ];
